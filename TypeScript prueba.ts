0dd19d592dc497abb3c56d3c05dde8e41a59e6bc
BAAL3113 <BAAL3113@GMAIL.COM>
2021-11-05 08:25:15 -0600

    Update TypeScript prueba.ts

--- TypeScript prueba.ts
+++ TypeScript prueba.ts
@@ -1,6 +1,22 @@
-var question="what's my name?";
+var question="what's my name?, " +"Answer me please";
 var answer=" my name is Johann";
-document.write(question);
-document.write(answer);
+document.write(question,"<br/>");
+document.write(answer,"<br/>");
 let name2="Johann";
-document.write(name2);
+document.write(name2+"<br/>");
+let age=37;
+document.write(age+"<br>");
+let isMarried=true;
+document.write(isMarried+"<br/>");
+let isMarried2=false;
+document.write(isMarried2+"<br/>");
+if(isMarried==true){
+    document.write("you are married <br/>");
+    document.write("FINALLY!!! <br/>");
+}
+else{
+    document.write("you are not married <br/>");
+
+}
+
+
