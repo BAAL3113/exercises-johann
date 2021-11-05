a9e11ef41c274d9c253609e46f606785f8453c87
BAAL3113 <BAAL3113@GMAIL.COM>
2021-11-04 19:01:02 -0600

    Update TypeScript prueba.ts

--- TypeScript prueba.ts
+++ TypeScript prueba.ts
@@ -2,12 +2,5 @@ var question="what's my name?";
 var answer=" my name is Johann";
 document.write(question);
 document.write(answer);
-//declare number
-let age: Number;
-age=25;
-//declare string
-let name2: string;
-name2="Johann";
+let name2="Johann";
 document.write(name2);
-
-//language: typescript
