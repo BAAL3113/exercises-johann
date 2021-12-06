9e9f9dd05a72c350d54d19867688d3b1964e50e9
BAAL3113 <BAAL3113@GMAIL.COM>
2021-12-06 03:43:03 -0600

    Update ExerciseExportImport2.js

--- ExerciseExportImport2.js
+++ ExerciseExportImport2.js
@@ -1,5 +1,5 @@
-import { add } from './ExerciseExportImport1.js';
-import { add2, sqrt, diag } from './ExerciseExportImportjs1.js';
+//import { add } from './ExerciseExportImport1.js';
+//import { add2, sqrt, diag } from './ExerciseExportImportjs1.js';
 //importScripts('ExerciseExportImportjs1.js');
 
 
@@ -12,42 +12,42 @@ import { add2, sqrt, diag } from './ExerciseExportImportjs1.js';
 // 5. Call the add3 function with the arguments 2 and 3
 // 6. Call the sqrt function with the argument 4
 // 7. Call the diag function with the argument [1, 2, 3]
-add(1, 2);
+/*add(1, 2);
 document.write(add(1, 2) + '<br>');
 add2(1, 2);
 console.log("🚀 ~ file: ExerciseExportImport2.js ~ line 18 ~ add2", add2(1, 2));
 document.write(add2(1, 2) + '<br>');
 document.write(sqrt(4) + '<br>');
-document.write(diag(4, 5) + '<br>');
+document.write(diag(4, 5) + '<br>');*/
 //Use * to Import Everything from a File
 document.write("Use * to Import Everything from a File");
-import * as myMath from './ExerciseExportImportjs1.js';
+/*import * as myMath from './ExerciseExportImportjs1.js';
 document.write(myMath.add(1, 2) + '<br>');
 document.write(myMath.add2(1, 2) + '<br>');
 document.write(myMath.sqrt(4) + '<br>');
-document.write(myMath.diag(4, 5) + '<br>');
+document.write(myMath.diag(4, 5) + '<br>');*/
 
 //Create an Export Fallback with export default
 document.write("Create an Export Fallback with export default");
 
-export default function add(x, y) {
+/*export default function add(x, y) {
     return x + y;
 }
 
 export default function(x, y) {
     return x + y;
-}
+}*/
 
 //Import a Default Export
 document.write("Import a Default Export");
-import add from './ExerciseExportImport2.js';
-document.write(add(1, 2) + '<br>');
+/*import add from './ExerciseExportImport2.js';
+document.write(add(1, 2) + '<br>');*/
 
 
 //Import a Named Export
 document.write("Import a Named Export");
-import { add } from './ExerciseExportImport2.js';
-document.write(add(1, 2) + '<br>');
+/*import { add } from './ExerciseExportImport2.js';
+document.write(add(1, 2) + '<br>');*/
 
 //Create a JavaScript Promise
 document.write("Create a JavaScript Promise");
